ffa837633059549224a711f99b2a045f82f299e4
Mahefa <abelmahefa@gmail.com>
2021-02-08 15:19:42 +0300

    update

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-  res.json(req.body);
+app.get('/', (req, res) => {
+  const articles = [];
+  articles.push("Post 1");
+  articles.push("Post 2");
+  res.json(articles);
 });
 
-app.listen(3000, () => console.log('server started'));
+app.post('/', (req, res) => {
+  res.json(req);
+});
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
