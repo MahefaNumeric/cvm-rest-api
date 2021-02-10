586a3e0adc847756569147977e08d588094893ce
Mahefa <abelmahefa@gmail.com>
2021-02-10 13:22:48 +0300

    update users.js, add Validation/UserValidation.js

---
+++ Validation/UserValidation.js
@@ -0,0 +1,12 @@
+
+function checkIdIfExist(idUser, connection){
+    const sql = `SELECT COUNT(id) FROM users WHERE id=${idUser}`;
+    connection.query(sql, (error, results, fields) => {
+        if(error) throw error;
+        console.log("Results: ", results);
+        return results[0];
+    });
+    return null;
+}
+
+module.exports.checkIdIfExist = checkIdIfExist;
