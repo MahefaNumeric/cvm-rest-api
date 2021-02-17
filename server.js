17a4b83474fd222fa1d464131b5423eecd164f7f
Mahefa Abel <abelmahefa@gmail.com>
2021-02-17 17:00:42 +0300

    update, backdoor

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParse = require("body-parser");
 const basicAuth = require("./Utils/BasicAuth");
+const cors = require("cors");
+
 const userController = require("./Controllers/UsersController");
 
 const app = express();
+
 app.use(bodyParse.json());
+// app.use(cors);
+
+const corsOption = {
+    origin:  "http://localhost"
+}
 
 app.use(basicAuth);
-app.use("/users", userController);
+app.use("/users", cors(corsOption), userController);
 
 app.listen(3000);
