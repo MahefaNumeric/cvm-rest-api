22e9aa130b04a4b5522da8e483baa993ed8b234a
Mahefa Abel <abelmahefa@gmail.com>
2021-02-18 22:28:43 +0300

    update

--- Utils/sql/cvm.sql
+++ Utils/sql/cvm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 10 fév. 2021 à 19:31
+-- Généré le : jeu. 18 fév. 2021 à 19:26
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -42,11 +42,92 @@ CREATE TABLE IF NOT EXISTS `companies` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `educations`
+-- Structure de la table `cv`
 --
 
-DROP TABLE IF EXISTS `educations`;
-CREATE TABLE IF NOT EXISTS `educations` (
+DROP TABLE IF EXISTS `cv`;
+CREATE TABLE IF NOT EXISTS `cv` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_template` int(11) DEFAULT NULL,
+  `title` varchar(68) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `cv_id_template` (`id_template`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cv_educations`
+--
+
+DROP TABLE IF EXISTS `cv_educations`;
+CREATE TABLE IF NOT EXISTS `cv_educations` (
+  `id_cv` int(11) NOT NULL,
+  `id_users_education` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_cv`,`id_users_education`),
+  KEY `cv_educations_id_users_education` (`id_users_education`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cv_experiences`
+--
+
+DROP TABLE IF EXISTS `cv_experiences`;
+CREATE TABLE IF NOT EXISTS `cv_experiences` (
+  `id_cv` int(11) NOT NULL,
+  `id_users_experience` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_cv`,`id_users_experience`),
+  KEY `cv_experiences_id_users_experience` (`id_users_experience`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cv_projects`
+--
+
+DROP TABLE IF EXISTS `cv_projects`;
+CREATE TABLE IF NOT EXISTS `cv_projects` (
+  `id_cv` int(11) NOT NULL,
+  `id_users_project` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_cv`,`id_users_project`),
+  KEY `cv_projects_id_users_project` (`id_users_project`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cv_skills`
+--
+
+DROP TABLE IF EXISTS `cv_skills`;
+CREATE TABLE IF NOT EXISTS `cv_skills` (
+  `id_cv` int(11) NOT NULL,
+  `id_users_skills` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_cv`,`id_users_skills`),
+  KEY `cv_skills_id_users_skills` (`id_users_skills`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `part_educations`
+--
+
+DROP TABLE IF EXISTS `part_educations`;
+CREATE TABLE IF NOT EXISTS `part_educations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(64) NOT NULL,
   `description` text NOT NULL,
@@ -60,11 +141,11 @@ CREATE TABLE IF NOT EXISTS `educations` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `experiences`
+-- Structure de la table `part_experiences`
 --
 
-DROP TABLE IF EXISTS `experiences`;
-CREATE TABLE IF NOT EXISTS `experiences` (
+DROP TABLE IF EXISTS `part_experiences`;
+CREATE TABLE IF NOT EXISTS `part_experiences` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(128) NOT NULL,
   `description` text NOT NULL,
@@ -76,11 +157,11 @@ CREATE TABLE IF NOT EXISTS `experiences` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `projects`
+-- Structure de la table `part_projects`
 --
 
-DROP TABLE IF EXISTS `projects`;
-CREATE TABLE IF NOT EXISTS `projects` (
+DROP TABLE IF EXISTS `part_projects`;
+CREATE TABLE IF NOT EXISTS `part_projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(64) NOT NULL,
   `description` text NOT NULL,
@@ -96,11 +177,11 @@ CREATE TABLE IF NOT EXISTS `projects` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `skills`
+-- Structure de la table `part_skills`
 --
 
-DROP TABLE IF EXISTS `skills`;
-CREATE TABLE IF NOT EXISTS `skills` (
+DROP TABLE IF EXISTS `part_skills`;
+CREATE TABLE IF NOT EXISTS `part_skills` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(48) NOT NULL,
   `description` varchar(128) NOT NULL,
@@ -112,19 +193,33 @@ CREATE TABLE IF NOT EXISTS `skills` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `preferences_users`
+--
+
+DROP TABLE IF EXISTS `preferences_users`;
+CREATE TABLE IF NOT EXISTS `preferences_users` (
+  `user_id` int(11) NOT NULL,
+  `template_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `users_preferences_template_id` (`template_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `socials_link`
 --
 
 DROP TABLE IF EXISTS `socials_link`;
 CREATE TABLE IF NOT EXISTS `socials_link` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `github_url` varchar(256) DEFAULT NULL,
   `linkedin_url` varchar(256) DEFAULT NULL,
   `skype_id` varchar(64) DEFAULT NULL,
   `website_url` varchar(256) DEFAULT NULL,
   `zoom_id` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`user_id`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -173,15 +268,73 @@ INSERT INTO `users` (`id`, `email`, `firstname`, `lastname`, `date_birth`, `auto
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users_preferences`
+-- Structure de la table `users_educations`
 --
 
-DROP TABLE IF EXISTS `users_preferences`;
-CREATE TABLE IF NOT EXISTS `users_preferences` (
-  `user_id` int(11) NOT NULL,
-  `template_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  KEY `users_preferences_template_id` (`template_id`)
+DROP TABLE IF EXISTS `users_educations`;
+CREATE TABLE IF NOT EXISTS `users_educations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_education` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_educations_unique_key` (`id_user`,`id_education`) USING BTREE,
+  KEY `users_educations_id_education` (`id_education`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_experiences`
+--
+
+DROP TABLE IF EXISTS `users_experiences`;
+CREATE TABLE IF NOT EXISTS `users_experiences` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_experience` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_experiences_unique` (`id_user`,`id_experience`) USING BTREE,
+  KEY `users_experiences_id_experience` (`id_experience`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_projects`
+--
+
+DROP TABLE IF EXISTS `users_projects`;
+CREATE TABLE IF NOT EXISTS `users_projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_project` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_projects_unique` (`id_user`,`id_project`) USING BTREE,
+  KEY `users_projects_id_project` (`id_project`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_skills`
+--
+
+DROP TABLE IF EXISTS `users_skills`;
+CREATE TABLE IF NOT EXISTS `users_skills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_skills` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_skills_unique` (`id_user`,`id_skills`) USING BTREE,
+  KEY `users_skills_id_skills` (`id_skills`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -189,18 +342,86 @@ CREATE TABLE IF NOT EXISTS `users_preferences` (
 --
 
 --
--- Contraintes pour la table `projects`
+-- Contraintes pour la table `cv`
 --
-ALTER TABLE `projects`
+ALTER TABLE `cv`
+  ADD CONSTRAINT `cv_id_template` FOREIGN KEY (`id_template`) REFERENCES `templates` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `cv_educations`
+--
+ALTER TABLE `cv_educations`
+  ADD CONSTRAINT `cv_educations_id_cv` FOREIGN KEY (`id_cv`) REFERENCES `cv` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `cv_educations_id_users_education` FOREIGN KEY (`id_users_education`) REFERENCES `users_educations` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `cv_experiences`
+--
+ALTER TABLE `cv_experiences`
+  ADD CONSTRAINT `cv_experiences_id_cv` FOREIGN KEY (`id_cv`) REFERENCES `cv` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `cv_experiences_id_users_experience` FOREIGN KEY (`id_users_experience`) REFERENCES `users_experiences` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `cv_projects`
+--
+ALTER TABLE `cv_projects`
+  ADD CONSTRAINT `cv_projects_id_cv` FOREIGN KEY (`id_cv`) REFERENCES `cv` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `cv_projects_id_users_project` FOREIGN KEY (`id_users_project`) REFERENCES `users_projects` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `cv_skills`
+--
+ALTER TABLE `cv_skills`
+  ADD CONSTRAINT `cv_skills_id_cv` FOREIGN KEY (`id_cv`) REFERENCES `cv` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `cv_skills_id_users_skills` FOREIGN KEY (`id_users_skills`) REFERENCES `users_skills` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `part_projects`
+--
+ALTER TABLE `part_projects`
   ADD CONSTRAINT `company_id` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
 
 --
--- Contraintes pour la table `users_preferences`
+-- Contraintes pour la table `preferences_users`
 --
-ALTER TABLE `users_preferences`
+ALTER TABLE `preferences_users`
   ADD CONSTRAINT `users_preferences_template_id` FOREIGN KEY (`template_id`) REFERENCES `templates` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION,
   ADD CONSTRAINT `users_preferences_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `socials_link`
+--
+ALTER TABLE `socials_link`
+  ADD CONSTRAINT `socials_link_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `users_educations`
+--
+ALTER TABLE `users_educations`
+  ADD CONSTRAINT `users_educations_id_education` FOREIGN KEY (`id_education`) REFERENCES `users_educations` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `users_educations_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `users_experiences`
+--
+ALTER TABLE `users_experiences`
+  ADD CONSTRAINT `users_experiences_id_experience` FOREIGN KEY (`id_experience`) REFERENCES `users_experiences` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `users_experiences_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `users_projects`
+--
+ALTER TABLE `users_projects`
+  ADD CONSTRAINT `users_projects_id_project` FOREIGN KEY (`id_project`) REFERENCES `part_projects` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `users_projects_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `users_skills`
+--
+ALTER TABLE `users_skills`
+  ADD CONSTRAINT `users_skills_id_skills` FOREIGN KEY (`id_skills`) REFERENCES `part_skills` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `users_skills_id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
