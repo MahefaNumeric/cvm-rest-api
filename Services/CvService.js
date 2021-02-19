92435c66996a1c51840a98c28ea66ec952897ec2
Mahefa Abel <abelmahefa@gmail.com>
2021-02-19 20:10:04 +0300

    update CvService.js

--- Services/CvService.js
+++ Services/CvService.js
@@ -31,12 +31,34 @@ class CvService{
     }
 
     /**
-     * 
-     * @param {*} idCv 
+     * Done
+     * @param {*} idCv : ID du CV
      * @param {*} format : PDF, HTML
-     * @param {*} mcbFinnished 
+     * @param {*} mcbFinnished : Callback
      */
     async generateCv(idCv, format, mcbFinnished){
+        const formatLC = String(format).toLowerCase();
+        if(formatLC == "html"){
+            return this.generateCvHtml(idCv, mcbFinnished);
+        }else if(formatLC == "pdf"){
+            return this.generateCvPdf(idCv, mcbFinnished);
+        }else{
+            return null;
+        }
+    }
+
+    async generateCvHtml(idCv, mcbFinnished){
+        const connMysql = require("../Configs/db.config");
+        const sql = `INSERT INTO 
+        users (firstname, lastname, date_birth, auto_desc) 
+        VALUE ('${pData.firstname}', '${pData.lastname}', '${pData.date_birth}', '${pData.auto_desc}')`;
+        connMysql.query(sql, (error, results, fields)=> {
+            if(error) throw error;
+            cbFinnished && cbFinnished(results);
+        });
+    }
+
+    async generateCvPdf(idCv, mcbFinnished){
         const connMysql = require("../Configs/db.config");
         const sql = `INSERT INTO 
         users (firstname, lastname, date_birth, auto_desc) 
